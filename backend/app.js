484393863f5dc8cd5d6f5106a1c38189d5edc236
NishantJain2710 <nishantjainyo@gmail.com>
2021-07-17 13:45:52 +0530

    adding  docker

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-    res.send('APIs ARE RUNNING ON PORT 5000')
+    res.send(`APIs ARE RUNNING ON PORT ${process.env.PORT || 3000}`)
 })
 
 //ROUTES
@@ -30,7 +30,7 @@ app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
-    console.log("server is running on port 5000")
+    console.log(`server is running on port ${PORT}`)
 })
